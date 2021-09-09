1d0c78b1c2a068a4aacebd57009cfc56221a7cde
lrr68 <lucca@luccaaugusto.xyz>
2021-09-09 18:58:37 -0300

    add functions to create widgets

--- src/cli.rs
+++ src/cli.rs
@@ -30,9 +30,69 @@ enum Event {
 	Tick,
 }
 
-fn display_notes_from_tag(tag: &Tag) { }
+fn build_tag_notes_list<'a>(tag: &Tag) -> List<'a> {
+	let notes_block = Block::default()
+		.borders(Borders::TOP) //TODO put border on config
+		.style(Style::default().fg(Color::White))
+		.title("-------".to_owned() + tag.get_name() + " notes:") //TODO put title on config
+		.border_type(BorderType::Rounded); //TODO put border style on config
 
-fn display_tags_menu(taglist: &Vec<Tag>) { }
+	let note_list = tag.get_note_list();
+	let items: Vec<_> = note_list
+		.iter()
+		.map(|note| {
+			ListItem::new(Spans::from(
+                vec![
+                    Span::styled(
+				        "    ".to_owned() + note.get_text(),
+				        Style::default(),
+			        )
+                ]
+            ))
+		})
+		.collect();
+
+	List::new(items)
+        .block(notes_block)
+        .highlight_style(
+		    Style::default()
+			.bg(Color::Yellow) //TODO put color on config
+			.fg(Color::Black) //TODO put color on config
+			.add_modifier(Modifier::BOLD), //TODO put modifier on config
+	    )
+}
+
+fn build_tags_menu(taglist: &Vec<Tag>, active_tag: usize) -> Tabs {
+	let menu = taglist
+		.iter()
+		.map(|t| {
+			Spans::from(vec![
+			Span::styled(
+				t.get_name(),
+				Style::default().fg(Color::White)//TODO put color on config
+				),
+			])
+		})
+	.collect();
+	Tabs::new(menu)
+		.select(active_tag.into())
+		.block(Block::default().title("-------".to_owned() + config::TABS_TITLE).borders(Borders::NONE)) //TODO put borders on config
+		.style(Style::default().fg(Color::White)) //TODO put color on config
+		.highlight_style(Style::default().fg(Color::Yellow)) //TODO put color on config
+		.divider(Span::raw("|")) //TODO put divider on config
+}
+
+fn configure_screen() -> Layout {
+    Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1) //TODO put margins on config
+        .constraints(
+            [
+                Constraint::Length(config::TABS_SIZE),
+                Constraint::Min(config::NOTES_SIZE),
+            ].as_ref()
+        )
+}
 
 pub fn run_anote(taglist: &mut Vec<Tag>, mut active_tag: usize) -> Result<(), Box<dyn std::error::Error>>  {
 
